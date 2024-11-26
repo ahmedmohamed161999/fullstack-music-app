146e074b07509f30d0d037b95dfe7a0bfca2db4b
Byte Store <aham161999@gmail.com>
2024-11-26 11:45:28 +0200

    url change

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ initializeSocket(httpServer);
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://music-app-1xis.vercel.app",
     credentials: true,
   })
 );
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
+      ? "https://music-app-gamma-fawn.vercel.app/api"
       : "/api",
 });
